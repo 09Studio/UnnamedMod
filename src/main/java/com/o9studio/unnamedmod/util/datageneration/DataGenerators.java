61bd940364095f276d453380c29d7493a90c2472
09 Studio <carlitosrvillanueva@gmail.com>
2023-03-04 13:41:40 +0100

    Data Generators

---
+++ src/main/java/com/o9studio/unnamedmod/util/datageneration/DataGenerators.java
@@ -0,0 +1,23 @@
+package com.o9studio.unnamedmod.util.datageneration;
+
+import com.o9studio.unnamedmod.UnnamedMod;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = UnnamedMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        generator.addProvider(true, ModLootTableProvider.create(packOutput));
+        generator.addProvider(true, new ModBlockStateProvider(packOutput, existingFileHelper));
+        generator.addProvider(true, new ModItemModelProvider(packOutput, existingFileHelper));
+    }
+}
---
+++ src/main/java/com/o9studio/unnamedmod/util/datageneration/ModBlockLootTables.java
@@ -0,0 +1,27 @@
+package com.o9studio.unnamedmod.util.datageneration;
+
+import com.o9studio.unnamedmod.core.ModBlocks;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    protected ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.NOP_BLOCK.get());
+
+        dropSelf(ModBlocks.AMBER_BLOCK.get());
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+}
