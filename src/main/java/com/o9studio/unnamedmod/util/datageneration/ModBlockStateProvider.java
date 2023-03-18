3eed41bc9718ae13b95859dd077a115f6b661e69
09 Studio <carlitosrvillanueva@gmail.com>
2023-03-18 16:01:38 +0100

    Crystal Blocks

--- src/main/java/com/o9studio/unnamedmod/util/datageneration/ModBlockStateProvider.java
+++ src/main/java/com/o9studio/unnamedmod/util/datageneration/ModBlockStateProvider.java
@@ -20,7 +20,6 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
         blockWithItem(ModBlocks.AMBER_BLOCK);
         blockWithItem(ModBlocks.BRONZE_BLOCK);
-        blockWithItem(ModBlocks.CUT_BRONZE);
         blockWithItem(ModBlocks.PERIDOT_BLOCK);
         blockWithItem(ModBlocks.RUBY_BLOCK);
         blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
