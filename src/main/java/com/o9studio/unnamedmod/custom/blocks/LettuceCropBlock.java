71bcee36038eb2188671aa9266d7a9652296ff28
09 Studio <carlitosrvillanueva@gmail.com>
2023-04-15 17:21:36 +0200

    Crops & Fixes

--- src/main/java/com/o9studio/unnamedmod/custom/blocks/LettuceCropBlock.java
+++ src/main/java/com/o9studio/unnamedmod/custom/blocks/LettuceCropBlock.java
@@ -1,18 +1,27 @@
 package com.o9studio.unnamedmod.custom.blocks;
 
 import com.o9studio.unnamedmod.core.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class LettuceCropBlock extends CropBlock {
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
 
     public LettuceCropBlock(Properties properties) {
         super(properties);
+    }
+
+    public VoxelShape getShape(BlockState blockState, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[blockState.getValue(this.getAgeProperty())];
     }
 
     @Override
--- src/main/java/com/o9studio/unnamedmod/custom/blocks/VanillaCropBlock.java
+++ src/main/java/com/o9studio/unnamedmod/custom/blocks/VanillaCropBlock.java
@@ -1,18 +1,27 @@
 package com.o9studio.unnamedmod.custom.blocks;
 
 import com.o9studio.unnamedmod.core.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class VanillaCropBlock extends CropBlock {
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
     public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 5);
 
     public VanillaCropBlock(Properties properties) {
         super(properties);
+    }
+
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[state.getValue(this.getAgeProperty())];
     }
 
     @Override
