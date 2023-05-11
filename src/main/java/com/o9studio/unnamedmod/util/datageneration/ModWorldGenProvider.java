9b469491fd89e1bbe277fbabbb7f26287e3a479d
09 Studio <carlitosrvillanueva@gmail.com>
2023-05-12 01:47:54 +0200

    Tree Gen

--- src/main/java/com/o9studio/unnamedmod/util/datageneration/ModWorldGenProvider.java
+++ src/main/java/com/o9studio/unnamedmod/util/datageneration/ModWorldGenProvider.java
@@ -2,6 +2,7 @@ package com.o9studio.unnamedmod.util.datageneration;
 
 import com.o9studio.unnamedmod.UnnamedMod;
 import com.o9studio.unnamedmod.world.features.ModConfiguredFeatures;
+import com.o9studio.unnamedmod.world.placement.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(UnnamedMod.MOD_ID));
