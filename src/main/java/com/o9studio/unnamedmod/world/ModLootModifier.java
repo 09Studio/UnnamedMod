bb5176292490585f87289f594f4767f1b788f6f8
09 Studio <carlitosrvillanueva@gmail.com>
2023-12-05 23:08:27 +0100

    Adding items to suspicious sand

--- src/main/java/com/o9studio/unnamedmod/world/ModLootModifier.java
+++ src/main/java/com/o9studio/unnamedmod/world/ModLootModifier.java
@@ -15,6 +15,9 @@ public class ModLootModifier {
     public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
             LOOT_MODIFIER_SERIALIZERS.register("add_item", AddItemModifier.CODEC);
 
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_SUS_SAND =
+            LOOT_MODIFIER_SERIALIZERS.register("add_sus_sand", AddSusSandModifier.CODEC);
+
 
     public static void register(IEventBus bus) {
         LOOT_MODIFIER_SERIALIZERS.register(bus);
